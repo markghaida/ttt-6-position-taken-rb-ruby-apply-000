d1afe48619fe09831ccc8d3fda541f02a7563ede
Mark G <markghaida@gmail.com>
2020-10-01 22:06:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] = " " || ""
   return false 
-elsif board[index] = "X" || "O"
+elsif board[index] = "X" || "O" || "x" || "o"
   return true 
 end 
 end
